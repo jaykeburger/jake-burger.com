3ae8d6b02a1198c26f47f3409b022e70b27ae8c5
Jake Burger <jaykegamer12@gmail.com>
2023-06-15 22:03:54 -0500

    feat(navbar) : update navbar comp

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import {  Link } from "react-router-dom";
+
 const Navbar= () =>{
   return (
   <div>
-    <li>
+    <ul>
+      <li>
       <Link to="/">Home</Link>
-    </li>
-    <li>
+      </li>
+      <li>
       <Link to="/about">About</Link>
-    </li>
+      </li>
+    </ul>
   </div>
   );
 }
