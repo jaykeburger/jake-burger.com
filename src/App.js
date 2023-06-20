873aec834c67a122f732de8128af5d007496ee2f
Jake Burger <jaykegamer12@gmail.com>
2023-06-20 00:29:35 -0500

    feat(navbar) added new routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import './App.css';
-import './Title'
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
-import Navbar from "./components/shared/Navbar"
-import Home from "./components/home/Home"
-import About from "./components/about/About"
-
-
+import Navbar from './components/shared/Navbar2';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+  
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar>
-        <Routes>
-          <Route path='/' element = {<Home/>} />
-          <Route path='/about' element = {<About/>} />
-        </Routes>
-      </Navbar>
-  </BrowserRouter>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={About} />
+      </Routes>
+    </Router>
   );
 }
-
+  
 export default App;
