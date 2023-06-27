aa9ffd4f778b8fad3e93f3feba7026047bc82d7b
Jake Burger <jaykegamer12@gmail.com>
2023-06-27 01:03:19 -0500

    feat(home): add home info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
 
 import React from 'react';
+import logo from '../assets/vwavegif.gif'
   
 const Home = () => {
   return (
     <div
       style={{
         display: 'flex',
-        justifyContent: 'Right',
-        alignItems: 'Right',
-        height: '100vh'
+        flexDirection: 'column',
+        justifyContent: 'Center',
+        alignItems: 'Center',
+        height: '100vh',
+        background: '#041129',
+        gap: '5%'
       }}
     >
-      <h1>Jake Burger</h1>
+      <div style={{ fontSize: 150, color:'white' }}>
+        Jake Burger
+      </div>
+      <img src={logo} style={{ width: "30%", height: "35%" }} alt="loading..."/>
     </div>
   );
 };
