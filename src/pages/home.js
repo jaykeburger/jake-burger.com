c93ef95e5de036a2a9106dcb4ab940c27aacb6e8
Jake Burger <jaykegamer12@gmail.com>
2023-07-04 02:12:06 -0500

    fix(home.css) changed div to class

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,11 @@ import React from 'react';
 import HomePageLogo from '../components/HomePageLogo';
 import HomePageTitle from '../components/HomePageTitle';
 import SocialMediaBar from '../components/SocialMediaBar';
+import '../styles/home.css';
   
 const Home = () => {
   return (
-    <div style={{background: '#041129', height:'100vh'}}>
+    <div class="home-style">
       <HomePageTitle/>
       <SocialMediaBar/>
       <HomePageLogo/>
