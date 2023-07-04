2108bf90a027253ede139bd0c7ef395b2dc57a3a
Jake Burger <jaykegamer12@gmail.com>
2023-07-04 01:38:52 -0500

    feat(title):added typewriter

--- src/components/HomePageTitle.js
+++ src/components/HomePageTitle.js
@@ -1,17 +1,50 @@
 import React from 'react';
-  
+import Typewriter from "typewriter-effect";
+
+
 const HomePageTitle = () => {
   return (
-    <div style={{        
+    <div 
+        style={{        
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'Center',
         gap: '5%',}}>
-        <div style={{ fontSize: 150, color:'white', fontFamily:'monospace' }}>
-        Jake Burger
-      </div>
+        <h1 style={{color:'white', fontFamily:'inherit', fontSize:'70px'}}><i>
+        <Typewriter
+            options={{
+                autoStart: true,
+                loop: true,
+                delay: 100
+                }}
+            onInit={(typewriter) => {
+                typewriter
+                .typeString("Jake Burger:")
+                .pause(20000)
+                .deleteAll()
+                .typeString("software developer")
+                .pause(20000)
+                .deleteAll()
+                .typeString("adventurer")
+                .pause(20000)
+                .start();
+            }}
+/>
+        </i></h1>
     </div>
   );
 };
   
 export default HomePageTitle;
+/*
+<Typewriter
+onInit={(typewriter) => {
+typewriter
+.typeString("Welcome to")
+.pause(20000)
+.deleteAll()
+.typeString("jake-burger.com")
+.start();
+}}
+/>
+*/
