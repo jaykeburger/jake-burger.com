d44985c15779801916aaa8546ad3b75f92a040fe
Jake Burger <jaykegamer12@gmail.com>
2023-07-25 00:41:04 -0500

    update(aboutwindow): added responsiveness

--- src/components/shared/AboutWindow.js
+++ src/components/shared/AboutWindow.js
@@ -11,15 +11,17 @@ const AboutWindow = () => {
                 <div class="close-tab2">-</div>
             </div>
             <div class="window-body">
-                <h1 className='about-title'>
+                <div className='about-section'>
+                    <h1 className='about-title'>
                     <img src={require('../../assets/spinningfries.gif')}/>
                     <i>About Me</i>
                     <img style={{transform:'scaleX(-1)'}} src={require('../../assets/burger.gif')}/>
                 </h1>
-                <p><i><b>Software Engineering @ Boeing</b></i></p>
-                <p><i><b>BS, Computer Science @ University of Houston</b></i></p>
-                <p><i><b>Minor, Mathematics @ University of Houston</b></i></p>
+                <p><i><b>Software Engineering @ Boeing 🚀</b></i></p>
+                <p><i><b>BS, Computer Science @ University of Houston 💻</b></i></p>
+                <p><i><b>Minor, Mathematics @ University of Houston 🧮</b></i></p>
                 <br></br>
+                <p><i><b>Who I am</b></i></p>
                 <p>I am Jake Burger, a student studying to be the best programmer I can be.</p>
                 <br></br>
                 <p><i><b>Code Coogs</b></i></p>
@@ -27,6 +29,10 @@ const AboutWindow = () => {
                 <br></br>
                 <p><i><b>Hobbies</b></i></p>
                 <p>Outside of school and work, I am an avid enjoyer of video games, hiking, and traveling.</p>
+                </div>
+                <div className='image-section'>
+                <img src={require('../../assets/photos/jakeburger.jpg')}/>
+                </div>
             </div>
         </div>
     </div>
